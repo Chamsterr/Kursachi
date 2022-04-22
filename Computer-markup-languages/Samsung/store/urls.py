82a9d2588cab3290c3e6006aec15c39a1e9238f4
Chamster <89588942+Chamsterr@users.noreply.github.com>
2022-04-22 11:28:18 +0300

    1

--- Computer-markup-languages/Samsung/store/urls.py
+++ Computer-markup-languages/Samsung/store/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import index
+from .views import index, category, product
 
+
+app_name = "store"
 urlpatterns = [
-    path('', index, name='index'),
+    path('', index, {'template_name': 'store/index.html'}, name='index'),
+    path('info/', index, {'template_name': 'store/info.html'}, name='info'),
+    path('catalog/<int:category_id>/', category,
+         {'template_name': 'store/catalog.html'}, name='catalog'),
+    path('product/<int:product_id>/', product,
+         {'template_name': 'store/product.html'}, name='product')
 ]
--- Computer-markup-languages/Samsung/store/views.py
+++ Computer-markup-languages/Samsung/store/views.py
@@ -1,12 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 
+categories = Category.objects.order_by().all()
+products_preview = {x: Product.objects.filter(category=x)[:3]
+                    for x in categories}
 
-def index(request):
-    products = Product.objects.all()
-    categories = Category.objects.all()
+
+def index(request, template_name):
     context = {
-        'products': products,
-        'categories': categories,
+        'products_preview': products_preview,
     }
-    return render(request, 'store/index.html', context)
+    return render(request, template_name, context)
+
+
+def category(request, template_name, category_id=None):
+    products_in = Product.objects.filter(category=category_id)
+    current_category = Category.objects.get(pk=category_id)
+    context = {
+        'products_in': products_in,
+        'categories': categories,
+        'products_preview': products_preview,
+        'current_category': current_category,
+    }
+    return render(request, template_name, context)
+
+
+def product(request, template_name, product_id):
+    product_ = Product.objects.get(pk=product_id)
+    context = {
+        'products_preview': products_preview,
+        'product': product_,
+    }
+    return render(request, template_name, context)
